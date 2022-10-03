c23ec92f418727d2f50a6ebc184e7e6a375aa027
CyberTML <cybertml@outlook.com>
2022-10-03 15:22:03 -0400

    Add project files.

---
+++ StoreFrontApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace StoreFrontApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
